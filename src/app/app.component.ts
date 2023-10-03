d10e4bea29ba9bc24239176f51332a915afbebf6
kry23 <infest.redstar@gmail.com>
2023-10-03 13:29:53 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ZonelessRouting} from "./shared/zone-less/zone-less-routing.service";
+import {CmsService} from "./cms.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'ecommint';
+  template$ = this.cmsService.getTemplate()
+  constructor(private  cmsService: CmsService,private route:Router,private active:ActivatedRoute) {
+    inject(ZonelessRouting).init();
+  }
 }
